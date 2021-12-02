d7a5394b44b094a9ba53c4c4f570b9a5066ffe59
Park Juhyung <majecty@gmail.com>
2021-12-03 02:36:22 +0900

    ♻️  rename example class name

--- java/netty/app/src/main/java/dev/juhyung/study/netty/eventloop/EventLoopExample.java
+++ java/netty/app/src/main/java/dev/juhyung/study/netty/eventloop/EventLoopExample.java
@@ -3,7 +3,7 @@ package dev.juhyung.study.netty.eventloop;
 import io.netty.channel.DefaultEventLoop;
 import java.util.concurrent.TimeUnit;
 
-public class EventLoopMain {
+public class EventLoopExample {
 
   public static void main(String[] args) throws InterruptedException {
     final var eventLoop = new DefaultEventLoop();

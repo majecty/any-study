da100a5221982835a46258643c3465edd84ab3d2
Park Juhyung <majecty@gmail.com>
2021-12-03 02:52:42 +0900

    ✨ use submit in Netty example

--- java/netty/app/src/main/java/dev/juhyung/study/netty/eventloop/EventLoopExample.java
+++ java/netty/app/src/main/java/dev/juhyung/study/netty/eventloop/EventLoopExample.java
@@ -7,11 +7,18 @@ public class EventLoopExample {
 
   public static void main(String[] args) throws InterruptedException {
     final var eventLoop = new DefaultEventLoop();
-    eventLoop.schedule(() -> {
-      System.out.println("in eventloop");
-      eventLoop.shutdownGracefully();
-    }, 100, TimeUnit.MILLISECONDS);
+    eventLoop.schedule(
+        () -> {
+          System.out.println("run in 100 millisec later");
+          eventLoop.shutdownGracefully();
+        },
+        100,
+        TimeUnit.MILLISECONDS);
 
+    eventLoop.submit(
+        () -> {
+          System.out.println("run immediately");
+        });
     eventLoop.awaitTermination(1, TimeUnit.SECONDS);
   }
 }
